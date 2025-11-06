0e9595e95c3a4acba5c6754aece35c123243905b
srinsubr <sri.subramaniam@gmail.com>
2025-11-06 13:58:22 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/event-brew-planner/",
+  
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
